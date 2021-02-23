939557ca3ef949f2143790b5dad0229c5865cb62
mm-sugar <74651452+mm-sugar@users.noreply.github.com>
2021-02-23 16:22:33 +0900

    ユーザー認証機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥]/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー。「」、]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー。「」、]+\z/ }
+  validates :birthday, presence: true
+
 end
