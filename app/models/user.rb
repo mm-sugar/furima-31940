f2e2af0b8d02331a87706550c7da65b48a6fea7b
mm-sugar <74651452+mm-sugar@users.noreply.github.com>
2021-02-24 18:45:19 +0900

    パスワード半角英数字混合設定へ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/ }
 
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥]/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥]/ }
