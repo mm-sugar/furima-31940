2cf0fa1a9c37c44aa2b339805491009acb28477d
mm-sugar <74651452+mm-sugar@users.noreply.github.com>
2021-02-27 21:15:35 +0900

    データベースの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_133058) do
-
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description", null: false
-    t.integer "category_id", null: false
-    t.integer "product_status_id", null: false
-    t.integer "shipping_burden_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "shipping_days_id", null: false
-    t.integer "price", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_products_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2021_02_16_144603) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -45,5 +30,4 @@ ActiveRecord::Schema.define(version: 2021_02_24_133058) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "products", "users"
 end
