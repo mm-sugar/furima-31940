6bfbe843b78a5d5255b0e36635decb934812fd4e
mm-sugar <74651452+mm-sugar@users.noreply.github.com>
2021-03-02 20:32:29 +0900

    ActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,10 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
   private
   def item_params
     params.require(:item).permit(:image).merge(user_id: current_iser.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,31 @@ class Item < ApplicationRecord
   has_many :comments
   has_one :purchase
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions do
+    belongs_to :category
+    belongs_to :product_status
+    belongs_to :shipping_burden
+    belongs_to :prefecture
+    belongs_to :shipping_days
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :product_status
+    validates :shipping_burden
+    validates :prefecture
+    validates :shipping_days
+  end
+
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :product_status_id
+    validates :shipping_burden_id
+    validates :prefecture_id
+    validates :shipping_days_id
+  end
 end
