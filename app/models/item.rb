8fed79ef698508f8090b8aaed25caee752460d6a
mm-sugar <74651452+mm-sugar@users.noreply.github.com>
2021-03-06 12:16:50 +0900

    単体テストコードの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,15 +4,10 @@ class Item < ApplicationRecord
   has_one :purchase
   has_one_attached :image
 
-  extend ActiveHash::Associations::ActiveRecordExtensions do
-    belongs_to :category
-    belongs_to :product_status
-    belongs_to :shipping_burden
-    belongs_to :prefecture
-    belongs_to :shipping_days
-  end
-
-  with_options numericality: { other_than: 1 } do
+  with_options presence: true do
+    validates :image
+    validates :name, length: {maximum: 40}
+    validates :description, length: {maximum: 1000}
     validates :category_id
     validates :product_status_id
     validates :shipping_burden_id
@@ -21,15 +16,20 @@ class Item < ApplicationRecord
     validates :price, inclusion: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
   end
 
+  extend ActiveHash::Associations::ActiveRecordExtensions do
+    belongs_to :category
+    belongs_to :product_status
+    belongs_to :shipping_burden
+    belongs_to :prefecture
+    belongs_to :shipping_days
+  end
 
-  with_options presence: true do
-    validates :name
-    validates :description
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :product_status_id
     validates :shipping_burden_id
     validates :prefecture_id
     validates :shipping_days_id
-    validates :image
   end
+
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {"化粧品"}
+    description {"化粧品"}
+    category_id {2}
+    product_status_id {3}
+    shipping_burden_id {2}
+    prefecture_id {2}
+    shipping_days_id{2}
+    price {1000}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpeg'), filename: 'test_image.jpeg')
+    end
   end
 end
