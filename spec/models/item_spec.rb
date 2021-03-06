27d27cf41f151de8659fafd2cb085815f92dc696
mm-sugar <74651452+mm-sugar@users.noreply.github.com>
2021-03-06 12:57:14 +0900

    テストコード"正常系""異常系"に書き分け

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,90 +6,96 @@ RSpec.describe Item, type: :model do
   end
   
   describe '商品の出品' do
-    it 'image、name、description、category_id、product_status_id、shipping_burden_id、prefecture_id、shipping_days_idとpriceが存在すれば出品できること' do
-      expect(@item).to be_valid
+    context '出品ができる場合' do
+      it 'image、name、description、category_id、product_status_id、shipping_burden_id、prefecture_id、shipping_days_idとpriceが存在すれば出品できること' do
+        expect(@item).to be_valid
+      end
+
+      it 'nameが40文字以下なら登録できること' do
+        @item.name = '12345678901234567890'
+        expect(@item).to be_valid
+      end
+
+      it 'descriptionが1000文字以内なら登録できること' do
+        @item.description = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        expect(@item).to be_valid
+      end
+
+      it 'priceが300円以上9999999円以内なら登録できること' do
+        @item.price = '2000'
+        expect(@item).to be_valid
+      end
+
     end
 
-    it 'imageが空では出品できないこと' do
-      @item.image = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Image can't be blank")
-    end
 
-    it 'nameが空では出品できないこと' do
-      @item.name = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Name can't be blank")
-    end
 
-    it 'descriptionが空では出品できないこと' do
-      @item.description= ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Description can't be blank")
-    end
+    context '出品できない場合' do
+      it 'imageが空では出品できないこと' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
 
-    it 'category_idが空では出品できないこと' do
-      @item.category_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Category can't be blank")
-    end
+      it 'nameが空では出品できないこと' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
 
-    it 'product_status_idが空では出品できないこと' do
-      @item.product_status_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Product status can't be blank")
-    end
+      it 'descriptionが空では出品できないこと' do
+        @item.description= ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
 
-    it 'shipping_burden_idが空では出品できないこと' do
-      @item.shipping_burden_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Shipping burden can't be blank")
-    end
+      it 'category_idが空では出品できないこと' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
 
-    it 'prefecture_idが空では出品できないこと' do
-      @item.prefecture_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include( "Prefecture can't be blank")
-    end
+      it 'product_status_idが空では出品できないこと' do
+        @item.product_status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product status can't be blank")
+      end
 
-    it 'shipping_days_idが空では出品できないこと' do
-      @item.shipping_days_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Shipping days can't be blank")
-    end
+      it 'shipping_burden_idが空では出品できないこと' do
+        @item.shipping_burden_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping burden can't be blank")
+      end
 
-    it 'priceが空では出品できないこと' do
-      @item.price = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Price can't be blank")
-    end
+      it 'prefecture_idが空では出品できないこと' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include( "Prefecture can't be blank")
+      end
 
-    it 'nameが40文字以下なら登録できること' do
-      @item.name = '12345678901234567890'
-      expect(@item).to be_valid
-    end
+      it 'shipping_days_idが空では出品できないこと' do
+        @item.shipping_days_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping days can't be blank")
+      end
 
-    it 'nameが41文字以上では登録できない' do
-      @item.name = '12345678901234567890123456789012345678901'
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
-    end
+      it 'priceが空では出品できないこと' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
 
-    it 'descriptionが1000文字以内なら登録できること' do
-      @item.description = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-      expect(@item).to be_valid
-    end
+      it 'nameが41文字以上では登録できない' do
+        @item.name = '12345678901234567890123456789012345678901'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
+      end
 
-    it 'descriptionが1000文字以上では登録できないこと' do
-      @item.description = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1'
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Description is too long (maximum is 1000 characters)")
-    end
-
-    it 'priceが300円以上9999999円以内なら登録できること' do
-      @item.price = '2000'
-      expect(@item).to be_valid
-    end
+      it 'descriptionが1000文字以上では登録できないこと' do
+        @item.description = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description is too long (maximum is 1000 characters)")
+      end
 
     it 'priceが300円以下では登録ができないこと' do
       @item.price = '299'
