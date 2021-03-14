62ed061f9d9233541291101166e60d4b072f11a4
mm-sugar <74651452+mm-sugar@users.noreply.github.com>
2021-03-14 11:38:55 +0900

    ルーティングとコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_item, only: [:show, :edit]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -19,11 +20,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
   end
 
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :product_status_id, :shipping_burden_id, :prefecture_id, :shipping_days_id, :price).merge(user_id: current_user.id)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 end
