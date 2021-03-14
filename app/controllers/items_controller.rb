1eed51cc79e68a8f7524f4eecc89254a5c863e16
mm-sugar <74651452+mm-sugar@users.noreply.github.com>
2021-03-14 15:54:16 +0900

    Updateアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :find_item, only: [:show, :edit]
+  before_action :find_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -21,8 +21,16 @@ class ItemsController < ApplicationController
 
   def show
   end
-
+  
   def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
