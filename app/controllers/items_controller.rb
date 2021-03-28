4eacc502e382bbd381afd230314bb1cbe45fa30a
mm-sugar <74651452+mm-sugar@users.noreply.github.com>
2021-03-28 20:24:27 +0900

    item_controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,6 +51,7 @@ class ItemsController < ApplicationController
   def block_item
     if @item.purchase.present? || @item.user_id == current_user.id
       redirect_to root_path
+    end
   end
   
 end
